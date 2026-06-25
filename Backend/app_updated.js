20f9ccfab915f5cb2ad10ee6c096bc989ff8eec0
kojozerus <minhkhang.15082005@gmail.com>
2026-06-25 10:13:44 +0700

    ok

--- Backend/app_updated.js
+++ Backend/app_updated.js
@@ -15,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // === KẾT NỐI MONGODB ===
+console.log('MONGODB_URI =', process.env.MONGODB_URI);
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/smarthub';
 mongoose.connect(MONGODB_URI)
   .then(() => console.log('✅ Kết nối MongoDB thành công'))
