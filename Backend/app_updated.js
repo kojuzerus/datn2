ab1207c285c6c56986c31b162802dddeb64969ce
kojozerus <minhkhang.15082005@gmail.com>
2026-06-25 10:20:30 +0700

    update

--- Backend/app_updated.js
+++ Backend/app_updated.js
@@ -15,9 +15,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // === KẾT NỐI MONGODB ===
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/smarthub';
+
 console.log("MONGODB_URI =", process.env.MONGODB_URI);
 console.log("Using URI =", MONGODB_URI);
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/smarthub';
+
 mongoose.connect(MONGODB_URI)
   .then(() => console.log('✅ Kết nối MongoDB thành công'))
   .catch(err => console.error('❌ Lỗi kết nối MongoDB:', err));
